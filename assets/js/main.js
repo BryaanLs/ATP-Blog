dc0ed8fc485a1d677f2f0077bae0e22bd8ef35b6
bryaanls <bryanluccas@hotmail.com>
2023-06-12 19:15:25 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const btnEmail = document.getElementById('sendEmail');
+const inputEmail = document.getElementById('email')
+
+function sendEmail() {
+    alert('Cadastrado com sucesso! (MENSAGEM FICTICIA)');
+}
+
+btnEmail.addEventListener('click', sendEmail);
