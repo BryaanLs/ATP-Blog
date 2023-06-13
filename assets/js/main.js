6130f71a7e3b9292096c61f83c871106b8d6461d
bryaanls <bryanluccas@hotmail.com>
2023-06-13 19:25:01 -0300

    deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 const btnEmail = document.getElementById('sendEmail');
-const inputEmail = document.getElementById('email')
+const inputEmail = document.getElementById('email');
+const openMenu = document.getElementById('openMenu');
+const menuMobile = document.getElementById('menu')
+
+function ativaMenu() {
+    menuMobile.classList.contains('menuMobile') ?
+        openMenu.innerText = 'X' :
+        openMenu.innerText = '☰';
+    menuMobile.classList.toggle('menuMobile');
+}
 
 function sendEmail() {
     alert('Cadastrado com sucesso! (MENSAGEM FICTICIA)');
 }
 
-btnEmail.addEventListener('click', sendEmail);
+openMenu.addEventListener('click', ativaMenu);
+
+let inputEmailLength = inputEmail.addEventListener('change', () => {
+    if (inputEmail.value.length > 0) {
+        btnEmail.addEventListener('click', sendEmail);
+    }
+})
+
+
+
